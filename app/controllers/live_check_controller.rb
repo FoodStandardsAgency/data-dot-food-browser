e05a5caa0450f3a61477ac791ca692117c59437b
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-02-15 14:10:22 +0000

    Don't cache live-check info

--- app/controllers/live_check_controller.rb
+++ app/controllers/live_check_controller.rb
@@ -1,6 +1,7 @@
 # Controller for automated liveness check
 class LiveCheckController < ApplicationController
   layout 'live_check'
+  before_filter :set_cache_headers
 
   def show
     api = CairnCatalogueBrowser::CatalogueApi.new
@@ -9,5 +10,13 @@ class LiveCheckController < ApplicationController
       message: 'present',
       dataset_count: api.datasets({}).count
     }
+  end
+
+  private
+
+  def set_cache_headers
+    response.headers['Cache-Control'] = 'no-cache, no-store'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Mon, 01 Jan 1990 00:00:00 GMT'
   end
 end
