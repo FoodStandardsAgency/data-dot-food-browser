41ca5bd59029e1fdae58e57959698fc47808133e
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-03-15 18:31:52 +0000

    Try to set page error status correctly

--- app/controllers/exception_controller.rb
+++ app/controllers/exception_controller.rb
@@ -1,9 +1,11 @@
 # Simple view state container to pass to exception view
 class ExceptionControllerViewState
   attr_reader :msg
+  attr_reader :status
 
-  def initialize(msg)
+  def initialize(msg, status)
     @msg = msg
+    @status = status
   end
 
   def show_highlighted?
@@ -21,7 +23,7 @@ class ExceptionController < ActionController::Base
     wrapped_exception = ActionDispatch::ExceptionWrapper.new(env, exception)
     @view_state = setup_view(wrapped_exception)
 
-    render 'error_page', status: wrapped_exception.status_code
+    render 'error_page', status: @view_state.status
   end
 
   private
@@ -44,20 +46,20 @@ class ExceptionController < ActionController::Base
     Rails.logger.debug("ServiceException #{svc_exception.message} \
       #{svc_exception.status} \
       #{svc_exception.service_message} from #{svc_exception.source}")
-    ExceptionControllerViewState.new(svc_exception.message)
+    ExceptionControllerViewState.new(svc_exception.message, svc_exception.status)
   end
 
   def setup_not_found_exception_view(ex)
     Rails.logger.debug(ex.exception)
     ExceptionControllerViewState.new(
-      "Not found: #{ex.exception.message}"
+      "Not found: #{ex.exception.message}", 404
     )
   end
 
   def setup_default_exception_view(ex)
     Rails.logger.debug(ex.exception)
     ExceptionControllerViewState.new(
-      "Sorry, something went wrong: #{ex.exception.message}"
+      "Sorry, something went wrong: #{ex.exception.message}", 500
     )
   end
 end
