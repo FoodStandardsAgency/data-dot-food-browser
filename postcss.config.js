42fdbf288e93edc33a2c3cfa1cb10dd00ca0f475
joescottdave <joseph.davies@outlook.com>
2019-11-08 11:38:58 +0000

    configure postcss requirements

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,11 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-color-function'),
+    require('postcss-custom-media'),
+    require('postcss-custom-properties'),
+    require('postcss-custom-selectors'),
+    require('postcss-mixins'),
+    require('postcss-nested'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
