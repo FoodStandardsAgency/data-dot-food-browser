1a5768b49b7b6fd371378eb5bbf4e7453addc55f
joescottdave <joseph.davies@outlook.com>
2020-04-03 15:23:22 +0100

    Make postcss plugins consistent with those used in pattern library

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,21 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-color-function'),
-    require('postcss-custom-media'),
-    require('postcss-custom-properties'),
-    require('postcss-custom-selectors'),
     require('postcss-mixins'),
+    require('postcss-cssnext')({
+      features: {
+        customProperties: {
+          preserve: false,
+          warnings: false
+        },
+        rem: false,
+        nesting: false
+      }
+    }),
     require('postcss-nested'),
+    require('postcss-normalize'),
     require('postcss-url'),
+    // Webpacker includes flexbugs-fixes and preset-env as standard
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
