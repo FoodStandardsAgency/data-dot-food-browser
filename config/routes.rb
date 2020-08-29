9bfab2cbc547434a381431cce77082405e9cf02c
Ian Dickinson <i.j.dickinson@gmail.com>
2020-08-29 15:59:32 +0100

    Another attempt at CSS image routing
    This attempt fixes the image route to use the deployment path, then
    redirects that at run time. It's a huge hack :(

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,18 @@ Rails.application.routes.draw do
 
   get '/live-check', to: 'live_check#show'
 
-  get '*unmatched_route', to: 'exception#render_404'
-
   get '/cookies', to: redirect(
     'https://www.food.gov.uk/about-us/data-and-policies/aboutsite/termsandconditions/privacypolicy/cookies',
     status: 302
   )
+
+  unless Rails.env.production?
+    # This route redirects the `/catalog` we need to inject into the hero image bg pattern
+    # in order to make the image resolve in production
+    get '/catalog/themes/:path1/:path2/:path3/:path4/:path5/:path6(.:format)',
+        to: redirect('/themes/%{path1}/%{path2}/%{path3}/%{path4}/%{path5}/%{path6}.%{format}',
+                     status: 302)
+  end
+
+  get '*unmatched_route', to: 'exception#render_404'
 end
